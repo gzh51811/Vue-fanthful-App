9ae53b86d6c2e8193ebe801a7de43b4db2eeecc7
Ann <446331381@qq.com>
2019-03-17 21:50:44 +0800

    列表页排序列表组件完成

---
+++ fanthful_project/src/component/godsListSortList.vue
@@ -0,0 +1,41 @@
+<template>
+  <ul class="sortList">
+    <li v-for="listName in listNames" :key="listName">{{listName}}</li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      listNames: [
+        "默认",
+        "按购买数量",
+        "按浏览器",
+        "按价格从高到低",
+        "按价格从低到高"
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+.sortList {
+  background: #fff;
+  position: relative;
+}
+.sortList::before {
+  content: "";
+  position: absolute;
+  display: inline-block;
+  border: 10px solid transparent;
+   border-bottom-color: #fff;
+   left: 50%;
+   transform: translateX(-50%);
+   top:-20px;
+}
+.sortList li {
+  height: 45px;
+  line-height: 45px;
+}
+</style>
+
--- fanthful_project/src/component/goodsListSortNav.vue
+++ fanthful_project/src/component/goodsListSortNav.vue
@@ -5,15 +5,30 @@
       <span>默认</span>
       <i class="up"></i>
     </div>
+    <div id="sortList" v-if="state">
+        <godsListSortList></godsListSortList>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+import godsListSortList from "./godsListSortList.vue";
+
+export default {
+    data(){
+        return {
+            state:false
+        }
+    },
+    components:{
+        godsListSortList
+    }
+};
 </script>
 <style scoped>
 .sortNav {
   height: 30px;
   padding: 7px 10px;
+  position: relative;
 }
 .sortNav > div {
   display: inline-block;
@@ -46,6 +61,15 @@ export default {};
   border-top-color: #fff;
   vertical-align: bottom;
   /* transform: rotateX(180deg); */
+}
+#sortList{
+    position: absolute;
+    z-index: 2;
+    right: 0;
+    top: 45px;
+    width: 50%;
+    background: #000;
+     padding: 7px 10px;
 }
 </style>
 
